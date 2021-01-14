ef3b91177d066a65fbb9c148a49d4aa18f945830
VoizTheCoder <77443234+VoizTheCoder@users.noreply.github.com>
2021-01-14 14:25:43 +0000

    Update index.js

--- index.js
+++ index.js
@@ -8,8 +8,3 @@ client.once('ready', () => {
 client.login(process.env.token);
 
 // Auto Moderation
-
-if (command === 'Fuck') {
-	-	message.channel.send('Dont Say That');
-	+	client.commands.get('ping').execute(message, args);
-	}

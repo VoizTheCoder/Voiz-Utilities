96c12a1c6a32c5569d7b7f90db042ad3d584352d
VoizTheCoder <77443234+VoizTheCoder@users.noreply.github.com>
2021-01-14 16:20:58 +0000

    Update index.js

--- index.js
+++ index.js
@@ -8,32 +8,13 @@ client.once('ready', () => {
 client.login(process.env.token);
 
 client.on('message', message => {
-	if (message.content === '!dev') {
-		message.channel.send('The Developer is Voiz');
+	if (message.content === '!devs') {
+		message.channel.send('The Scripter is Voiz#7752 & Tester is Mushy#6486');
 	}
 });
-client.on (command === "kick") {
-	if (!message.member.hasPermission('KICK_MEMBERS'))
-		return message.channel.send("Insufficient permissions (Requires permission `Kick members`)").then(msg => {
-	msg.delete({ timeout: 30000 })
-})
-	const member = message.mentions.members.first();
-	if (!member)
-		return message.channel.send("You have not mentioned a user").then(msg => {
-	msg.delete({ timeout: 30000 })
-})
-	if (!member.kickable)
-		return message.channel.send("This user is unkickable").then(msg => {
-	msg.delete({ timeout: 30000 })
-})
-	const reason = args.slice(1).join(" ")
-	if (member) {
-		if (!reason) return member.kick().then(member => {
-			message.channel.send(`${member.user.tag} was kicked, no reason was provided`);
-		})
 
-		if (reason) return member.kick().then(member => {
-			message.channel.send(`${member.user.tag} was kicked for ${reason}`);
-		})
+client.on('message', message => {
+	if (message.content === '!help') {
+		message.channel.send('For Extra Support Join The Voiz Utilities Discord Server: https://discord.gg/N3sbJbrXyr');
 	}
-}
+});

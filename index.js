14bf859a2f67b045723c9ac5f1b1236ae5437451
VoizTheCoder <77443234+VoizTheCoder@users.noreply.github.com>
2021-01-14 14:47:00 +0000

    Update index.js

--- index.js
+++ index.js
@@ -12,10 +12,3 @@ client.on('message', message => {
 		message.channel.send('The Developer is Voiz');
 	}
 });
-client.on("message", (message) => {
-	//let's use something like a spam variable for 10 or more messages sent within 5000ms
-	if(message.content === spam) {
-		message.reply("Warning: Spamming in this server is forbidden.");
-		console.log(message.author.username + " (" + message.author.id + ") has sent 10 messages or more in 5 seconds in " + message.channel.name + ".");
-	  }
-	});

8f71ffb1326f9a1d1f6e3b14a2ba6f87e09a6e41
VoizTheCoder <77443234+VoizTheCoder@users.noreply.github.com>
2021-01-14 18:20:45 +0000

    Update index.js

--- index.js
+++ index.js
@@ -15,12 +15,18 @@ client.on('message', message => {
 
 client.on('message', message => {
 	if (message.content === '!help') {
-		message.channel.send('For Extra Support Join The Voiz Utilities Discord Server: https://discord.gg/4HU6qreYAu');
+		message.channel.send('For Extra Support Join The Voiz Utilities Discord Server: https://discord.gg/4HU6qreYAu \n \n Type `!commands` to see the commands');
 	}
 });
 
 client.on('message', message => {
 	if (message.content === '!servers') {
 		message.channel.send('Servers This Bot is used in is: \n Voiz Dream World: https://discord.gg/EXzPbrp6yZ \n Voiz Utilities Bot Hub: https://discord.gg/4HU6qreYAu \n \n With More To Come!  ');
+	}
+});
+
+client.on('message', message => {
+	if (message.content === '!commands') {
+		message.channel.send(' Last Updated 14/01/21 \n The Current Commands Are: \n `!devs` Get a list of the bots Scripters and testers \n `!servers` Get A list of the servers the bot is in \n `!help` Get Help. ');
 	}
 });

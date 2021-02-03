51f38bb87c0cae83bd3dace734a597fffedc9fdc
VoizTheCoder <77443234+VoizTheCoder@users.noreply.github.com>
2021-02-03 20:11:43 +0000

    Update index.js

--- index.js
+++ index.js
@@ -115,3 +115,14 @@ module.exports = {
     }
 }
 
+module.exports = {
+    name: "restart",
+    category: "owner",
+    run: async (client, message, args) => {
+        if (message.author.id !== '185957154606284800') {
+            return message.channel.send(`You cannot use this command!`)
+        }
+        await message.channel.send(`Restarting bot...`)
+        process.exit();
+    }
+}

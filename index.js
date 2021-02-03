835796899e2aa10fb1cd7ff533241305d2073c10
VoizTheCoder <77443234+VoizTheCoder@users.noreply.github.com>
2021-02-03 20:02:14 +0000

    Update index.js

--- index.js
+++ index.js
@@ -159,3 +159,33 @@ client.on('message', message => {
 		message.channel.send('Sorry. The Command `!meme` is currently a feature for the future. If you want to receive updates. Join The Voiz Utilities Server! \n https://discord.gg/4HU6qreYAu');
 	}
 }); 
+
+const { MessageEmbed } = require('discord.js');
+
+module.exports = {
+    name: "lock",
+    category: "moderation",
+    run: async (client, message, args) => {
+        const channels = message.guild.channels.cache.filter(ch => ch.type !== 'category');
+        if (args[0] === 'on') {
+            channels.forEach(channel => {
+                channel.updateOverwrite(message.guild.roles.everyone, {
+                    SEND_MESSAGES: false
+                }).then(() => {
+                    channel.setName(channel.name += `🔒`)
+                })
+            })
+            return message.channel.send('locked all channels');
+        } else if (args[0] === 'off') {
+            channels.forEach(channel => {
+                channel.updateOverwrite(message.guild.roles.everyone, {
+                    SEND_MESSAGES: true
+                }).then(() => {
+                        channel.setName(channel.name.replace('🔒', ''))
+                    }
+                )
+            })
+            return message.channel.send('unlocked all channels')
+        }
+    }
+}

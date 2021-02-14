ea977113accc56f7738d9c3c50b53a4adfab6cbe
VoizTheCoder <77443234+VoizTheCoder@users.noreply.github.com>
2021-02-14 21:32:53 +0000

    Update index.js

--- index.js
+++ index.js
@@ -9,91 +9,22 @@ client.once('ready',  () => {
 
 client.login(process.env.token);
 
-exports.run = async(client, msg, args) => {
-    if(!msg.member.hasPermission('KICK_MEMBERS')) return msg.reply('You can\'t use that!');
+if (command === 'ban') {
+	if (args.length < 2) {
+		return message.reply('Please mention the user you want to ban and specify a ban reason.');
+	}
 
-    var user = msg.mentions.users.first();
-    if(!user) return msg.reply('You didn\'t mention anyone!');
+	const user = getUserFromMention(args[0]);
+	if (!user) {
+		return message.reply('Please use a proper mention if you want to ban someone.');
+	}
 
-    var member;
+	const reason = args.slice(1).join(' ');
+	try {
+		await message.guild.members.ban(user, { reason });
+	} catch (error) {
+		return message.channel.send(`Failed to ban **${user.tag}**: ${error}`);
+	}
 
-    try {
-        member = await msg.guild.members.fetch(user);
-    } catch(err) {
-        member = null;
-    }
-
-    if(!member) return msg.reply('They aren\'t in the server!');
-    if(member.hasPermission('MANAGE_MESSAGES')) return msg.reply('You cannot kick this person!');
-
-    var reason = args.splice(1).join(' ');
-    if(!reason) return msg.reply('You need to give a reason!');
-
-    var channel = msg.guild.channels.cache.find(c => c.name === 'potato');
-
-    var log = new Discord.MessageEmbed()
-    .setTitle('User Kicked')
-    .addField('User:', user, true)
-    .addField('By:', msg.author, true)
-    .addField('Reason:', reason)
-    channel.send(log);
-
-    var embed = new Discord.MessageEmbed()
-    .setTitle('You were kicked!')
-    .setDescription(reason);
-
-    try {
-        await user.send(embed);
-    } catch(err) {
-        console.warn(err);
-    }
-
-    member.kick(reason);
-
-    msg.channel.send(`**${user}** has been kicked by **${msg.author}**!`);
-}
-
-exports.run = async(client, msg, args) => {
-    if(!msg.member.hasPermission('BAN_MEMBERS')) return msg.reply('You can\'t use that!');
-
-    var user = msg.mentions.users.first();
-    if(!user) return msg.reply('You didn\'t mention anyone!');
-
-    var member;
-
-    try {
-        member = await msg.guild.members.fetch(user);
-    } catch(err) {
-        member = null;
-    }
-
-    if(member){
-        if(member.hasPermission('MANAGE_MESSAGES')) return msg.reply('You cannot ban this person!');
-    }
-
-    var reason = args.splice(1).join(' ');
-    if(!reason) return msg.reply('You need to give a reason!');
-
-    var channel = msg.guild.channels.cache.find(c => c.name === 'potato');
-
-    var log = new Discord.MessageEmbed()
-    .setTitle('User Banned')
-    .addField('User:', user, true)
-    .addField('By:', msg.author, true)
-    .addField('Reason:', reason)
-    channel.send(log);
-
-    var embed = new Discord.MessageEmbed()
-    .setTitle('You were banned!')
-    .setDescription(reason);
-
-    try {
-        await user.send(embed);
-    } catch(err) {
-        console.warn(err);
-    }
-
-    msg.guild.members.ban(user); // This should not be user.id like I said in my video. I made a mistake. Sorry! :)
-
-    msg.channel.send(`**${user}** has been banned by **${msg.author}**!`);
+	return message.channel.send(`Successfully banned **${user.tag}** from the server!`);
 }

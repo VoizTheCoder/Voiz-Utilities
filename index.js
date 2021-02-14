06f32a96c54df90beba6d7b7c4f0a08d893329f1
VoizTheCoder <77443234+VoizTheCoder@users.noreply.github.com>
2021-02-14 20:02:59 +0000

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const client = new Discord.Client();
 
 client.once('ready',  () => {
     console.log('Ready');
-	client.user.setActivity(`${client.guilds.cache.size} Servers!`, {type:'WATCHING'}); 
+	client.user.setActivity(`For The Rule Breakers`, {type:'WATCHING'}); 
 }); 
 
 client.login(process.env.token);

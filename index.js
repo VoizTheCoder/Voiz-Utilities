21d4ef31dee820f21bc39c884a340419ec4ba37d
VoizTheCoder <77443234+VoizTheCoder@users.noreply.github.com>
2021-02-14 23:27:00 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const prefix = "!";
-const role = guild.roles.find("name", "@everyone");
+const role = guild.roles.find("name", "Member");
 
 client.once('ready',  () => {
     console.log('Ready');

1927dd8389ae0ff166d539a0fff0a2950e54a183
VoizTheCoder <77443234+VoizTheCoder@users.noreply.github.com>
2021-02-14 21:51:50 +0000

    Update index.js

--- index.js
+++ index.js
@@ -8,3 +8,10 @@ client.once('ready',  () => {
 }); 
 
 client.login(process.env.token);
+
+client.on('message', message => {
+	if (message.content === '!serverinfo') {
+		message.channel.send('Server name: ${message.guild.name}\nTotal members: ${message.guild.memberCount}\n Created At: ${message.guild.createdAt}\n Server Region: ${message.guild.region} ');
+	}
+});
+

17890ef8d68d0a2bd03cc9d068f5a81d0a34d58b
VoizTheCoder <77443234+VoizTheCoder@users.noreply.github.com>
2021-02-14 21:34:00 +0000

    Update index.js

--- index.js
+++ index.js
@@ -8,23 +8,3 @@ client.once('ready',  () => {
 }); 
 
 client.login(process.env.token);
-
-if (command === 'ban') {
-	if (args.length < 2) {
-		return message.reply('Please mention the user you want to ban and specify a ban reason.');
-	}
-
-	const user = getUserFromMention(args[0]);
-	if (!user) {
-		return message.reply('Please use a proper mention if you want to ban someone.');
-	}
-
-	const reason = args.slice(1).join(' ');
-	try {
-		await message.guild.members.ban(user, { reason });
-	} catch (error) {
-		return message.channel.send(`Failed to ban **${user.tag}**: ${error}`);
-	}
-
-	return message.channel.send(`Successfully banned **${user.tag}** from the server!`);
-}

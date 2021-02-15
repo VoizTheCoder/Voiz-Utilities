c279464e790cb345cefff0f6a37f26713710b1a4
VoizTheCoder <77443234+VoizTheCoder@users.noreply.github.com>
2021-02-15 22:38:30 +0000

    Update index.js

--- index.js
+++ index.js
@@ -94,15 +94,4 @@ client.on("message", async message => {
     }
 
 })
-if (msg.content.startsWith("!kick ")) {
-if (msg.member.hasPermission("KICK_MEMBERS")) 
-    if (msg.members.mentions.first()) {
-        try {
-            msg.members.mentions.first().kick();
-        } catch {
-            msg.reply("I do not have permissions to kick " + msg.members.mentions.first());
-        }
-    } else {
-        msg.reply("You do not have permissions to kick " + msg.members.mentions.first());
-    }
-}
+

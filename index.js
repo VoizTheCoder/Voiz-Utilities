41c60339d53992c5551693f12eef94b8f024fbe7
VoizTheCoder <77443234+VoizTheCoder@users.noreply.github.com>
2021-02-15 22:10:48 +0000

    Update index.js

--- index.js
+++ index.js
@@ -95,10 +95,6 @@ client.on("message", async message => {
 
 })
 
-let messageArray = message.content.split(" ");
-let args = messageArray.slice(1);
-let cmd = messageArray[0];
-
  if(cmd === '?tempmute'){
 	if(message.member.hasPermission('MANAGE_MESSAGES')) {
 		var member = message.guild.member(message.mentions.users.first() || message.guild.members.cache.get(args[0]));

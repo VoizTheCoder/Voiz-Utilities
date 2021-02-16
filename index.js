59f62ab2f9978453418d1bad6b5e80fb8b0acbd6
VoizTheCoder <77443234+VoizTheCoder@users.noreply.github.com>
2021-02-16 12:21:55 +0000

    Update index.js

--- index.js
+++ index.js
@@ -126,4 +126,26 @@ client.on('message', message =>{
         break;
     }
 })
-     
+if (message.content.startsWith(`${prefix}lockdown`)) {
+    if (!message.member.hasPermission(["MANAGE_CHANNELS"])) return message.reply('You can\'t use this command!')
+    const channels = message.guild.channels.cache.filter(ch => ch.type !== 'category');
+    if (args[1] === 'on') {
+        channels.forEach(channel => {
+            channel.updateOverwrite(message.guild.roles.everyone, {
+                SEND_MESSAGES: false
+            }).then(() => {
+                channel.setName(channel.name += `🔒`)
+            })
+        })
+        return message.channel.send('Locked all channels');
+    } else if (args[1] === 'off') {
+        channels.forEach(channel => {
+            channel.updateOverwrite(message.guild.roles.everyone, {
+                SEND_MESSAGES: true
+            }).then(() => {
+                channel.setName(channel.name.replace('🔒', ''))
+            })
+        })
+        return message.channel.send('Unlocked all channels')
+    }
+}

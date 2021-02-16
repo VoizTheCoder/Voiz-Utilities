79e58ff8b9b1e08ab25557e020bcad74dca9b420
VoizTheCoder <77443234+VoizTheCoder@users.noreply.github.com>
2021-02-16 12:20:56 +0000

    Update index.js

--- index.js
+++ index.js
@@ -127,38 +127,3 @@ client.on('message', message =>{
     }
 })
      
-client.on('message', message =>{
- 
-    let args = message.content.substring(prefix.length).split(" ")
-
-    if(!message.member.hasPermission("MANAGE_CHANNELS")) return;
-
-    if(db.fetch(`lock.${message.channel.id}`)) return message.reply("This channel already locked")
-
-    let msg = await message.channel.send("Loading...")
-
-    try {
-        db.set(`lock.${message.channel.id}`,message.author.id)
-        message.channel.updateOverwrite(message.guild.roles.cache.find(e => e.name.toLowerCase().trim() == "@everyone"),{
-            SEND_MESSAGES:false,
-            ADD_REACTIONS:false
-        })
-        msg.edit(":white_check_marker:")
-
-    }catch(e){
-        message.channel.send(e)
-    }
-}
-
-exports.conf = {
-    enabled: true,
-    guildOnly: true,
-    aliase:[]
-}
-
-exports.help = {
-    name:"lock",
-    description:"Empty",
-    usage:"embed",
-    category:"moderation"
-}

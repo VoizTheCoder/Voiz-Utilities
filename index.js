9e39fa0aa7aa998d7c5f2b83cf949a7698a02f4f
VoizTheCoder <77443234+VoizTheCoder@users.noreply.github.com>
2021-02-16 13:26:42 +0000

    Update index.js

--- index.js
+++ index.js
@@ -126,3 +126,21 @@ client.on('message', message =>{
         break;
     }
 })
+
+const Discord = module.require('discord.js');
+module.exports.run = async (bot, message, args) => {
+if(!args[1]) return message.channel.send("Spefify the length of slowmode in seconds! (1-21600 Seconds)")
+let duration = args[1]
+message.channel.setRateLimitPerUser(duration)
+  .catch(() => {
+message.channel.send("Failed to set slowmode in this channel, check your slowmode length.")
+  })
+message.channel.send("I have set the slowmode in this channel to " + duration + " seconds!")
+}
+module.exports.config = {
+name: "slowmode",
+description: "Sets the slowmode.",
+usage: ">slowmode",
+accessableby: "Moderators",
+aliases: ['slowmode']
+}

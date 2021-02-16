1b7e6c22cb72e8cb9b2b4eb2cb8401be02fb371d
VoizTheCoder <77443234+VoizTheCoder@users.noreply.github.com>
2021-02-16 09:47:48 +0000

    Update index.js

--- index.js
+++ index.js
@@ -98,25 +98,3 @@ client.on("message", async message => {
     }
 
 })
-
-client.on("message", async message => {})
-
-
-let messageArray = message.content.split(" ")
-    let args = messageArray.slice(1);
-
-    let cmd = messageArray[0];
-
-if(cmd === "!kick") {
-    let toBan = message.mentions.members.first() || message.guild.members.cache.get(args[0]) || message.guild.members.cache.find(x => x.user.username.toLowerCase() === args.slice(0).join(" ") || x.user.username === args[0]);
-
-    if (!message.member.hasPermission("KICK_MEMBERS")) return message.channel.send("You need permissions!") 
-    if (!message.guild.me.hasPermission("KICK_MEMBERS")) return message.channel.send("Bot need permissions!") 
-
-    const reason = args[1] || "There was no reason!";
-
-    toBan.kick({
-        reason: reason
-    })
-    message.channel.send(`${toBan} has been kicked from the server!\nReason: ${reason}`)
-}
